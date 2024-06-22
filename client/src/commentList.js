f8972d150e3b0164f1e7b4468aa235c9872c8641
Rishi Gulati <rishisg2@rishis-air-3.lan>
2024-06-22 14:55:20 -0700

    changing comment content in react app based on comment status

--- client/src/commentList.js
+++ client/src/commentList.js
@@ -3,11 +3,21 @@ import React from 'react';
 
 // React Component
 const CommentListComponent = ({comments}) => {
-
+    
+    let content;
     // Display cards for each retrieved post
     const renderedComments = comments.map(comment => {
+        if (comment.status === "accepted"){
+            content = comment.content;
+        }
+        else if (comment.status === "pending"){
+            content = "This comment is awaiting moderation";
+        }
+        else if (comment.status === "rejected"){
+            content = "This comment was hidden by the moderators";
+        }
         return (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>{content}</li>
         );
     });
 
